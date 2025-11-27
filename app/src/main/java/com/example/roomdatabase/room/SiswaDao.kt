105f51934f902b12d38d259309b67793be303899
verss21 <arjunafrizal01@gmail.com>
2025-11-27 19:46:57 +0700

    membuat interface untuk menampilkan data siswa

---
+++ app/src/main/java/com/example/roomdatabase/room/SiswaDao.kt
@@ -0,0 +1,16 @@
+package com.example.roomdatabase.room
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface SiswaDAO {
+    @Query(value = "SELECT * from tblSiswa ORDER BY nama ASC")
+    fun getAllSiswa() : Flow<List<Siswa>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(siswa : Siswa)
+}
