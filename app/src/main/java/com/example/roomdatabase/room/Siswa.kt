8a7bad5fcc0005ee4aebb72615d015b030cdf69e
verss21 <arjunafrizal01@gmail.com>
2025-11-27 19:44:31 +0700

    Menambahkan entity Siswa untuk tabel tblSiswa

--- app/src/main/java/com/example/roomdatabase/room/Siswa.kt
+++ app/src/main/java/com/example/roomdatabase/room/Siswa.kt
@@ -1,7 +1,8 @@
 package com.example.roomdatabase.room
 
+import androidx.room.Entity
 import androidx.room.PrimaryKey
-
+@Entity(tableName = "tblSiswa")
 data class Siswa(
     @PrimaryKey(autoGenerate = true)
     val id : Int = 0,
