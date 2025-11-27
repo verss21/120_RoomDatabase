0e5afa367e8803bac5d91ebe95b302773cccd46c
verss21 <arjunafrizal01@gmail.com>
2025-11-27 19:47:55 +0700

    Deklarasi class HomeViewModel dan companion object

---
+++ app/src/main/java/com/example/roomdatabase/viewmodel/HomeViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.roomdatabase.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.roomdatabase.repositori.RepositoriSiswa
+import com.example.roomdatabase.room.Siswa
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlin.collections.toList
+
+class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+
