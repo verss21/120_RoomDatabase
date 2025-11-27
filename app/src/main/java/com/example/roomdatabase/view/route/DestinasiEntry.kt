a57e99e5a7e6da385c84d38444db6bd4316185b4
verss21 <arjunafrizal01@gmail.com>
2025-11-27 19:42:36 +0700

    Menambahkan destinasi navigasi DestinasiEntry

--- app/src/main/java/com/example/roomdatabase/view/route/DestinasiEntry.kt
+++ app/src/main/java/com/example/roomdatabase/view/route/DestinasiEntry.kt
@@ -1,8 +1,8 @@
 package com.example.roomdatabase.view.route
 
 import com.example.roomdatabase.R
-import com.example.roomdatabase.R.view.route.DestinasiNavigasi
-object DestinasiEntry {
+
+object DestinasiEntry: DestinasiNavigasi {
     override val route = "item_entry"
     override val titleRes = R.string.entry_siswa
 }
