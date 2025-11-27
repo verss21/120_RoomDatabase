9bc57d002f478c168db043025c43799d20e780f3
verss21 <arjunafrizal01@gmail.com>
2025-11-27 18:37:55 +0700

    Menambahkan value

---
+++ app/src/main/java/com/example/roomdatabase/view/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.roomdatabase.view.route
+
+import com.example.roomdatabase.R
+import com.example.roomdatabase.R.view.route.DestinasiNavigasi
+object DestinasiEntry {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
