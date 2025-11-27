8fae0a705fabe34a747b3e586b3eafa3ed9feb48
verss21 <arjunafrizal01@gmail.com>
2025-11-27 19:34:41 +0700

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,9 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.runtime)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.ui)
     testImplementation(libs.junit)
 
     androidTestImplementation(libs.androidx.junit)
