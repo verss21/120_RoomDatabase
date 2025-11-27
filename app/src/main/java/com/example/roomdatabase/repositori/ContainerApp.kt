8922f0092e0b004d948c8a32ddde58bef0043c45
verss21 <arjunafrizal01@gmail.com>
2025-11-27 19:39:01 +0700

    deklarasi class ContainerDataApp

--- app/src/main/java/com/example/roomdatabase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/roomdatabase/repositori/ContainerApp.kt
@@ -9,3 +9,11 @@ interface ContainerApp {
     val repositoriSiswa : RepositoriSiswa
 }
 
+class ContainerDataApp(private val context: Context):
+        ContainerApp {
+            override val repositoriSiswa: RepositoriSiswa by lazy {
+                OfflineRepositoriSiswa(
+                DatabaseSiswa.getDatabase(context).siswaDao())
+            }
+        }
+
--- app/src/main/java/com/example/roomdatabase/repository/ContainerApp.kt
+++
@@ -1,2 +0,0 @@
-package com.example.roomdatabase.repository
-
