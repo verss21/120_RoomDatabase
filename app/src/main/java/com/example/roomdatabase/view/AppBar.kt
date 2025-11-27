d2e4487a81d203bd0e17a17dad4d994131917c37
verss21 <arjunafrizal01@gmail.com>
2025-11-27 19:36:25 +0700

    membuat center aligned TopAppBar

--- app/src/main/java/com/example/roomdatabase/view/AppBar.kt
+++ app/src/main/java/com/example/roomdatabase/view/AppBar.kt
@@ -21,4 +21,18 @@ fun SiswaTopAppBar(
     modifier: Modifier = Modifier,
     scrollBehavior: TopAppBarScrollBehavior? = null,
     navigateUp: () -> Unit = {}
-)
+) {
+    CenterAlignedTopAppBar(title = { Text(title) },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back)
+                    )
+                }
+            }
+        })
+}
